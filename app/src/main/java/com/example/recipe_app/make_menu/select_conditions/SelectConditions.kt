e33efae4b130f7b87ac58e46dcd8ee34c9823433
daikiitoh <2200262@ecc.ac.jp>
2022-11-08 12:22:39 +0900

    mergeミスを修正

--- app/src/main/java/com/example/recipe_app/make_menu/select_conditions/SelectConditions.kt
+++ app/src/main/java/com/example/recipe_app/make_menu/select_conditions/SelectConditions.kt
@@ -1,12 +1,17 @@
 package com.example.recipe_app.make_menu.select_conditions
 
-import androidx.compose.foundation.layout.Column
+import android.util.Log
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.material.ExtendedFloatingActionButton
 import androidx.compose.material.Tab
 import androidx.compose.material.TabRow
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun SelectConditions(
@@ -36,6 +41,16 @@ fun SelectConditions(
                 )
             }
         }
+
+        LazyRow(
+            contentPadding = PaddingValues(all = 16.dp),
+            horizontalArrangement = Arrangement.spacedBy(16.dp),
+        ) {
+            item { ClearButton() }
+            item { SearchButton({}) }
+        }
+
+        Spacer(modifier = Modifier.padding(56.dp))
     }
 }
 
@@ -66,3 +81,33 @@ private val ConditionTabs = listOf(
     ConditionTab.SelectTagsTab,
     ConditionTab.SelectIngredientsTab
 )
+
+@Composable
+fun SearchButton(onSearchClicked: () -> Unit) {
+    Box(/*modifier = Modifier.fillMaxSize()*/) {
+        ExtendedFloatingActionButton(
+            modifier = Modifier
+                .padding(all = 16.dp)
+                .align(alignment = Alignment.BottomEnd),
+            onClick = onSearchClicked, // →レシピ一覧画面(→レシピ画面)
+            text = {
+                Text(
+                    text = "検索",
+                )
+            }
+        )
+    }
+}
+
+@Composable
+fun ClearButton() {
+    Box(/*modifier = Modifier.fillMaxSize()*/) {
+        ExtendedFloatingActionButton(
+            modifier = Modifier
+                .padding(all = 16.dp)
+                .align(alignment = Alignment.BottomEnd),
+            onClick = { Log.d("Button", "onClick") },
+            text = { Text("クリア") }
+        )
+    }
+}
