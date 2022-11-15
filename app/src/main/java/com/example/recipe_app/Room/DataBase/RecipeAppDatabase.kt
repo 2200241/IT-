2e464b6335251f5e55ace9ce9084b682e84f8ddf
2200241 <2200241@ecc.ac.jp>
2022-11-15 12:25:35 +0900

    localDB Room追加 gradle追記 リポジトリとデータベースは一緒のフォルダにまとめてます

---
+++ app/src/main/java/com/example/recipe_app/Room/DataBase/RecipeAppDatabase.kt
@@ -0,0 +1,38 @@
+package com.example.recipe_app.Room.DataBase
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.recipe_app.Room.Allergy.Allergy
+import com.example.recipe_app.Room.Allergy.AllergyDao
+import com.example.recipe_app.Room.Favorite.Favorite
+import com.example.recipe_app.Room.Favorite.FavoriteDao
+
+@Database(entities = arrayOf(Favorite::class, Allergy::class), version = 1, exportSchema = false)
+abstract class RecipeAppDatabase: RoomDatabase() {
+
+    abstract fun favoriteDao(): FavoriteDao
+    abstract fun AllergyDao(): AllergyDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: RecipeAppDatabase? = null
+
+        fun getDatabase(
+            context: Context
+        ): RecipeAppDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    RecipeAppDatabase::class.java,
+                    "recipe_app_database"
+                )
+                    .fallbackToDestructiveMigration()
+                    .build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/recipe_app/Room/Favorite/Favorite.kt
@@ -0,0 +1,12 @@
+package com.example.recipe_app.Room.Favorite
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "Favorites")
+data class Favorite(
+    @PrimaryKey(autoGenerate = true) val favorite_id: Int,
+    @ColumnInfo(name = "recepi_id") val recepi_id: Int,
+    @ColumnInfo(name = "name") val name: String?
+)
