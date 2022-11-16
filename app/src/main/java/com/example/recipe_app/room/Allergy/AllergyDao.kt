d07c1121bd958b26ed3ca44fa74ec6c68d4e61fe
2200241 <2200241@ecc.ac.jp>
2022-11-16 11:23:22 +0900

    localDB Room追加 gradle追記 リポジトリとデータベースは一緒のフォルダにまとめてます

---
+++ app/src/main/java/com/example/recipe_app/room/Allergy/AllergyDao.kt
@@ -0,0 +1,25 @@
+package com.example.recipe_app.room.Allergy
+
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.example.recipe_app.room.Allergy.Allergy
+
+@Dao
+interface AllergyDao {
+    @Query("SELECT * FROM allergies")
+    fun readAllData(): LiveData<List<Allergy>>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE) 
+    suspend fun addAllergy(allergy: Allergy)
+
+    @Query("delete from allergies where allergy_id =:allergy_id")
+    suspend fun deleteAllergy(allergy_id: Int)
+
+    @Update
+    suspend fun updateAllergy(allergy: Allergy)
+
+    @Query("delete from allergies")
+    suspend fun deleteAllAllergies()
+
+}
---
+++ app/src/main/java/com/example/recipe_app/room/Favorite/FavoriteRepository.kt
@@ -0,0 +1,41 @@
+package com.example.recipe_app.room.Favorite
+
+import com.example.recipe_app.room.Favorite.Favorite
+import com.example.recipe_app.room.Favorite.FavoriteDao
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+class FavoriteRepository(private val favoriteDao : FavoriteDao) {
+
+    //全件取得
+    fun getAllData() = favoriteDao.readAllData()
+
+    //追加
+    suspend fun addFavorite(favorite: Favorite) {
+        withContext(Dispatchers.IO){
+            favoriteDao.addFavorite(favorite)
+        }
+    }
+
+    //指定したIDを削除
+    suspend fun deleteFavorite(favorite_id: Int){
+        withContext(Dispatchers.IO) {
+            favoriteDao.deleteFavorite(favorite_id)
+        }
+    }
+
+    //更新
+    suspend fun updateFavorite(favorite: Favorite){
+        withContext(Dispatchers.IO) {
+            favoriteDao.updateFavorite(favorite)
+        }
+    }
+
+    //全件削除
+    suspend fun deleteAllFavorites(){
+        withContext(Dispatchers.IO) {
+            favoriteDao.deleteAllFavorites()
+        }
+    }
+}
+
