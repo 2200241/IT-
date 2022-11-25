c531170aeb342dba59e095d91ef72c1bbcd3d29d
daikiitoh <2200262@ecc.ac.jp>
2022-11-25 12:06:01 +0900

    Favoriteのプロパティを変更

--- app/src/main/java/com/example/recipe_app/repositories/TestRepository.kt
+++ app/src/main/java/com/example/recipe_app/repositories/TestRepository.kt
@@ -1,5 +1,7 @@
 package com.example.recipe_app.repositories
 
+import com.example.recipe_app.data.Favorites
+import com.example.recipe_app.data.Menu
 import com.example.recipe_app.data.Recipe
 import com.github.michaelbull.result.Ok
 import com.github.michaelbull.result.Result
@@ -8,9 +10,17 @@ import javax.inject.Inject
 
 interface TestRepository {
     suspend fun fetchRecipes(conditions: String): Result<List<Recipe>, String>
+    suspend fun fetchFavorites(): Result<Favorites, String>
+    suspend fun addFavoriteRecipe(recipe: Recipe): Result<String, String>
+    suspend fun addFavoriteMenu(menu: Menu): Result<String, String>
+    suspend fun fetchMenus(): Result<List<Menu>, String>
+    suspend fun addMenu(menu: Menu): Result<String, String>
 }
 
 class TestRepositoryImpl @Inject constructor(): TestRepository {
+    private var testMenus = emptyList<Menu>()
+    private var testFavorites = Favorites()
+
     override suspend fun fetchRecipes(conditions: String): Result<List<Recipe>, String> {
         val list = conditions.split("&")
         val a = list.map { Recipe(id = it, title = "タイトルA@$it", thumb = "http://hoge/thumb/$it.png") }
@@ -21,5 +31,29 @@ class TestRepositoryImpl @Inject constructor(): TestRepository {
 
         delay(1000)
         return Ok(a + b + c + d + e)
+    }
+
+    override suspend fun fetchFavorites(): Result<Favorites, String> {
+        delay(1000)
+        return Ok(testFavorites)
+    }
+
+    override suspend fun addFavoriteRecipe(recipe: Recipe): Result<String, String> {
+        testFavorites.recipes + recipe
+        return Ok("Successed")
+    }
+
+    override suspend fun addFavoriteMenu(menu: Menu): Result<String, String> {
+        testFavorites.menus + menu
+        return Ok("Successed")
+    }
+
+    override suspend fun fetchMenus(): Result<List<Menu>, String> {
+        return Ok(testMenus)
+    }
+
+    override suspend fun addMenu(menu: Menu): Result<String, String> {
+        testMenus + menu
+        return Ok("Successed")
     }
 }
